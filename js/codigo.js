2d7a1718a9926379d027ce825de41dd8170c1816
Breno Miguel <100931830+brenomiguel@users.noreply.github.com>
2023-03-11 14:16:07 -0300

    Add files via upload

---
+++ js/codigo.js
@@ -0,0 +1,21 @@
+function trocap1(){
+    window.location.href = "index.html"
+
+}
+
+function trocap2(){
+
+    window.location.href = "gp1.html"
+}
+
+function trocap3(){
+    window.location.href = "pg2.html"
+}
+
+function trocap4(){
+    window.location.href = "pg3.html"
+}
+
+function trocap5(){
+    window.location.href = "pg4.html"
+}
