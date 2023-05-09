6c858195bc780ffa93ea3a2184ecc8f87852e7bf
Breno Miguel <100931830+brenomiguel@users.noreply.github.com>
2023-05-09 10:50:57 -0300

    Add files via upload

---
+++ exemplo01/teste.js
@@ -0,0 +1,28 @@
+//var nome = prompt("Digite seu nome: ")
+//console.log(nome)
+
+//alert("bem vindo " + nome)
+
+var a = 15
+
+if (true){
+    let a = 10
+    console.log(a)
+
+}
+
+console.log(a) 
+
+//Calculando a soma de dois numeros
+
+var n1= parseInt(prompt("Digite n1;")) 
+var n2= parseInt(prompt("Digite n2:"))
+
+var soma = n1+n2
+
+//console.log("A soma é: " + soma)
+//tamplete string
+//console.log(`A soma é ${soma}`)
+document.write(`<p>A soma é ${soma}</p>`)
+
+//console.log(typeof n1)
